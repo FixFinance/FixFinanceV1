4a390ff137ce2744bf77d24109279dec53308bcc
MaxFeldman1 <mackx@protonmail.com>
2021-01-04 19:05:57 -0600

    add big math for calculation of pool constant

--- contracts/libraries/BigMath.sol
+++ contracts/libraries/BigMath.sol
@@ -3,19 +3,20 @@ pragma solidity >=0.5.0;
 import "./ABDKMath64x64.sol";
 import "./SignedSafeMath.sol";
 
-contract BigMath {
+library BigMath {
   using ABDKMath64x64 for int128;
   using SignedSafeMath for int256;
 
   /*
-    we keep error bound here such that we won't exceed 35 iterations in the power series and 
+    we keep error bound here such that we won't exceed 35 iterations in the power series 
     epsilon ~ 0.000000000108420217248550443400745280086994171142578125 * 2**64
 
   */
   int128 private constant epsilon = 2000000000;
 
-  int256[] private addition_series_data_array = [
-      3.532638989211429e21, 8.123575301925086e21,
+  /*
+  int256[] private constant addition_series_data_array = (int256[] memory)([
+      int256( 3.532638989211429e21 ), 8.123575301925086e21,
       1.914547520851727e22, 4.597351009781708e22,
       1.1199767955048595e23, 2.7595467055933305e23,
       6.861762397213119e23, 1.7190984485914473e24,
@@ -37,10 +38,130 @@ contract BigMath {
       1.4653938605367742e37, 3.894739269536102e37,
       1.0356610276733984e38, 2.755261428205564e38,
       7.333397495026503e38, 1.9527053469860518e39
-  ];
+  ]);
+  */
 
-  int128 constant MAX = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
+  int128 private constant MAX = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+
+  function addition_series_data(uint8 _index) public pure returns (int256) {
+      /*
+      because this is a library we cannot use state data, also there is not support for constant arrays in solidity libraries
+      thus we must return the correct value using a series of if statements
+
+      addition_series_data_array = [
+        int256( 3.532638989211429e21), int256( 8.123575301925086e21),
+        int256( 1.914547520851727e22), int256( 4.597351009781708e22),
+        int256( 1.1199767955048595e23), int256( 2.7595467055933305e23),
+        int256( 6.861762397213119e23), int256( 1.7190984485914473e24),
+        int256( 4.334170479517456e24), int256( 1.0986161322763805e25),
+        int256( 2.797703108389213e25), int256( 7.1534708728861e25),
+        int256( 1.835618393612818e26), int256( 4.7252538897488815e26),
+        int256( 1.2198312830141285e27), int256( 3.1570619510569525e27),
+        int256( 8.189734001592899e27), int256( 2.128967126497089e28),
+        int256( 5.5450007070812015e28), int256( 1.4467672526275159e29),
+        int256( 3.780936367779663e29), int256( 9.895777734762272e29),
+        int256( 2.5935973811247214e30), int256( 6.806354363991228e30),
+        int256( 1.788329890071965e31), int256( 4.704000104541168e31),
+        int256( 1.2386324902270515e32), int256( 3.264711636491927e32),
+        int256( 8.612886291792749e32), int256( 2.2742106798878982e33),
+        int256( 6.00992892534685e33), int256( 1.5894430796449978e34),
+        int256( 4.2066807430832585e34), int256( 1.1141313708614602e35),
+        int256( 2.9527056022310355e35), int256( 7.830266715673091e35),
+        int256( 2.077745864657038e36), int256( 5.516396839118708e36),
+        int256( 1.4653938605367742e37), int256( 3.894739269536102e37),
+        int256( 1.0356610276733984e38), int256( 2.755261428205564e38),
+        int256( 7.333397495026503e38), int256( 1.9527053469860518e39)
+      ]
+    */
+    if (_index < 32) {
+      if (_index < 16) {
+        if (_index < 8) {
+          if (_index < 4) {
+            if (_index < 2) {
+              if (_index < 1) return int256( 3.532638989211429e21);
+              return int256( 8.123575301925086e21);
+            }
+            if (_index < 3) return int256( 1.914547520851727e22);
+            return int256( 4.597351009781708e22);
+          }
+          if (_index < 6) {
+            if (_index < 5) return int256( 1.1199767955048595e23);
+            return int256( 2.7595467055933305e23);
+          }
+          if (_index < 7) return int256( 6.861762397213119e23);
+          return int256( 1.7190984485914473e24);
+        }
+        if (_index < 12) {
+          if (_index < 10) {
+            if (_index < 9) return int256( 4.334170479517456e24);
+            return int256( 1.0986161322763805e25);
+          }
+          if (_index < 11) return int256( 2.797703108389213e25);
+          return int256( 7.1534708728861e25);
+        }
+        if (_index < 14) {
+          if (_index < 13) return int256( 1.835618393612818e26);
+          return int256( 4.7252538897488815e26);
+        }
+        if (_index < 15) return int256( 1.2198312830141285e27);
+        return int256( 3.1570619510569525e27);
+      }
+      if (_index < 24) {
+        if (_index < 20) {
+          if (_index < 18) {
+            if (_index < 17) return int256( 8.189734001592899e27);
+            return int256( 2.128967126497089e28);
+          }
+          if (_index < 19) return int256( 5.5450007070812015e28);
+          return int256( 1.4467672526275159e29);
+        }
+        if (_index < 22) {
+          if (_index < 21) return int256( 3.780936367779663e29);
+          return int256( 9.895777734762272e29);
+        }
+        if (_index < 23) return int256( 2.5935973811247214e30);
+        return int256( 6.806354363991228e30);
+      }
+      if (_index < 28) {
+        if (_index < 26) {
+          if (_index < 25) return int256( 1.788329890071965e31);
+          return int256( 4.704000104541168e31);
+        }
+        if (_index < 27) return int256( 1.2386324902270515e32);
+        return int256( 3.264711636491927e32);
+      }
+      if (_index < 30) {
+        if (_index < 29) return int256( 8.612886291792749e32);
+        return int256( 2.2742106798878982e33);
+      }
+      if (_index < 31) return int256( 6.00992892534685e33);
+      return int256( 1.5894430796449978e34);
+    }
+    if (_index < 40) {
+      if (_index < 36) {
+        if (_index < 34) {
+          if (_index < 33) return int256( 4.2066807430832585e34);
+          return int256( 1.1141313708614602e35);
+        }
+        if (_index < 35) return int256( 2.9527056022310355e35);
+        return int256( 7.830266715673091e35);
+      }
+      if (_index < 38) {
+        if (_index < 37) return int256( 2.077745864657038e36);
+        return int256( 5.516396839118708e36);
+      }
+      if (_index < 39) return int256( 1.4653938605367742e37);
+      return int256( 3.894739269536102e37);
+    }
+    if (_index < 42) {
+      if (_index < 41) return int256( 1.0356610276733984e38);
+      return int256( 2.755261428205564e38);
+    }
+    if (_index < 43) return int256( 7.333397495026503e38);
+    return int256( 1.9527053469860518e39);
+  }
 
   function isOutOfBounds(int256 x) internal pure returns (bool) {
     /*
@@ -122,7 +243,7 @@ contract BigMath {
     return int256(g.add(_x.abs().ln()).sub(_x.exp().mul(Sn)));
   }
 
-  function arg_add_series_return(int128 k, int128 Sn, int128 xx) internal view returns (int256) {
+  function arg_add_series_return(int128 k, int128 Sn, int128 xx) internal pure returns (int256) {
       /*
         we divide the exponent by 2 and then square the result is because
         the math library we use is bound to 128 bits
@@ -134,13 +255,13 @@ contract BigMath {
       int256 a = exp * Sn;
       require(a/exp == Sn, "multiplication overflow");
       a >>= 64;
-      int256 b = addition_series_data_array[uint8(k>>64)-7];
+      int256 b = addition_series_data(uint8(k>>64)-7);
       int256 c = a + b;
       require((b >= 0 && c >= a) || (b < 0 && c < a), "addition overflow");
       return c;
   }
 
-  function Argument_Addition_Series_Ei(int128 x) public view returns (int256) {
+  function Argument_Addition_Series_Ei(int128 x) public pure returns (int256) {
      int128 k = ((x + (1<<63)) >> 64) << 64;
      int128 j = 0;
      int128 xx = k;
@@ -202,7 +323,7 @@ contract BigMath {
   }
 
 
-  function Ei(int128 x) public view returns (int256) {
+  function Ei(int128 x) public pure returns (int256) {
     require(x >= int(-19 * 2**64) && x <= int(87 * 2**64), "not in range");
     if (x < int(-5 * 2**64) || x > int(50 * 2**64))
       return Continued_Fraction_Ei(x);
@@ -214,7 +335,7 @@ contract BigMath {
 
 
   /*
-    @Description: find the pool constant given current pool reserves
+    @Description: find the pool constant for the YT-U amm given current pool reserves
 
     @param U: amount of pool reserves of Underlying asset, inflated by 64 bits
     @param Y: amount of pool reserves of Yield Tokens, inflated by 64 bits
@@ -222,7 +343,7 @@ contract BigMath {
     @param r: amount of time remaining / anchor, inflated by 64 bits
     @param APYo: the apy returned from the oracle inflated by 64 bits
   */
-  function PoolConstant(int256 U, int256 Y, int256 L, int256 r, int128 APYo) public view returns (int256) {
+  function YT_U_PoolConstant(int256 U, int256 Y, int256 L, int256 r, int128 APYo) public pure returns (int256) {
     /*
       K = U - (L* r * ln(APYo) * Ei(-L*r*ln(APYo)/Y) + Y*(APYo**(-L*r/Y) -1))
       K = U + (-L* r * ln(APYo) * Ei(-L*r*ln(APYo)/Y) - Y*(APYo**(-L*r/Y) -1))
@@ -248,7 +369,7 @@ contract BigMath {
     return U.add( ((termMulLog >> 64).mul(ei) >> 64).sub(Y.mul(APYoexp) >> 64) );
   }
 
-  function PoolConstantMinusU(int256 Y, int256 L, int256 r, int128 APYo) public view returns (int256) {
+  function YT_U_PoolConstantMinusU(int256 Y, int256 L, int256 r, int128 APYo) public pure returns (int256) {
     /*
       K = U - (L* r * ln(APYo) * Ei(-L*r*ln(APYo)/Y) + Y*(APYo**(-L*r/Y) -1))
       K = U + (-L* r * ln(APYo) * Ei(-L*r*ln(APYo)/Y) - Y*(APYo**(-L*r/Y) -1))

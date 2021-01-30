dba288af6be15c77a0b2061489bc8b18bbffdf87
MaxFeldman1 <mackx@protonmail.com>
2021-01-30 12:28:18 -0600

    add ATknToYT function in SwapRouter contract

--- contracts/SwapRouter.sol
+++ contracts/SwapRouter.sol
@@ -38,9 +38,33 @@ contract SwapRouter is ISwapRouter {
 		ch.transfer(msg.sender, _out);
 	}
 
-/*
-	function ATknToYT(address _capitalHandlerAddress, uint _amount, uint _maxATkn) external override {}
+	function ATknToYT(address _capitalHandlerAddress, int128 _amountYT, uint _maxATkn) external override {
+		_amountYT++;	//account for rounding error when transfering funds out of YTamm
+		require(_amountYT > 0);
+		ICapitalHandler ch = ICapitalHandler(_capitalHandlerAddress);
+		organizer _org = org;
+		IERC20 aToken = IERC20(_org.capitalHandlerToAToken(_capitalHandlerAddress));
+		IAaveWrapper aw = IAaveWrapper(_org.aTokenWrappers(address(aToken)));
+		IYTamm amm = IYTamm(_org.YTamms(_capitalHandlerAddress));
+		IYieldToken yt = IYieldToken(ch.yieldTokenAddress());
 
+		uint _amtATkn = amm.ReserveQuoteToYT(_amountYT);
+		//remove possibility for problems due to rounding error
+		uint _amtTransfer = _amtATkn + 100;
+		require(_amtTransfer <= _maxATkn, "Required AToken in is Greater than _maxATkn");
+
+		aToken.transferFrom(msg.sender, address(this), _amtTransfer);
+		aToken.approve(address(aw), _amtTransfer);
+		uint _amountWrapped = aw.deposit(address(this), _amtTransfer);
+		aw.approve(_capitalHandlerAddress, _amountWrapped);
+		ch.depositWrappedToken(address(this), _amountWrapped);
+		ch.approve(address(amm), _amtTransfer);
+		yt.approve(address(amm), _amountWrapped);
+		amm.TakeQuote(_maxATkn, int128(_amountYT), false);
+		yt.transfer(msg.sender, yt.balanceOf(address(this)));
+	}
+
+/*
 	function LiquidateAllToATkn(address _capitalHandler) external override {}
 
 	function SwapZCBtoYT(address _capitalHandlerAddress, uint _amountYT) external override {}
--- contracts/interfaces/ISwapRouter.sol
+++ contracts/interfaces/ISwapRouter.sol
@@ -2,9 +2,9 @@ pragma solidity >=0.6.0;
 
 interface ISwapRouter {
 	function ATknToZCB(address _capitalHandlerAddress, uint _amount) external;
-/*
-	function ATknToYT(address _capitalHandlerAddress, uint _amount) external;
 
+	function ATknToYT(address _capitalHandlerAddress, int128 _amount, uint _maxATkn) external;
+/*
 	function LiquidateAllToATkn(address _capitalHandler) external;
 
 	function SwapZCBtoYT(address _capitalHandlerAddress, uint _amountYT) external;

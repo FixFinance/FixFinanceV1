c40e38184120ed61acef1ad5ee266b926862fa0f
MaxFeldman1 <mackx@protonmail.com>
2021-03-08 00:28:10 -0600

    emit event on CapitalHandler deployment

--- contracts/CapitalHandlerDeployer.sol
+++ contracts/CapitalHandlerDeployer.sol
@@ -2,15 +2,21 @@ pragma solidity >=0.6.0;
 import "./CapitalHandler.sol";
 
 contract CapitalHandlerDeployer {
-	address public addr;
+	event CapitalHandlerDeployment(
+		address _wrapperAddress,
+		uint64 _maturity,
+		address _yieldTokenDeployer,
+		address _bondMinterAddress
+	);
 
 	function deploy(
 		address _aw,
 		uint64 _maturity,
 		address _yieldTokenDeployer,
 		address _bondMinterAddress
-		) public returns (address) {
-
-		return address(new CapitalHandler(_aw, _maturity, _yieldTokenDeployer, _bondMinterAddress));
+		) public returns (address addr) {
+		addr = address(new CapitalHandler(_aw, _maturity, _yieldTokenDeployer, _bondMinterAddress));
+		
+		emit CapitalHandlerDeployment(_aw, _maturity, _yieldTokenDeployer, _bondMinterAddress);
 	}
 }

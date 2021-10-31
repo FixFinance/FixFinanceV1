c461d9bb69becd631a12f59feb5c457146dccc0c
Steven Li <steven4354@gmail.com>
2021-10-31 13:48:41 -0700

    code to generate typechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
+require("hardhat-typechain");
+require("@nomiclabs/hardhat-web3");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,8 +22,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
 //  solidity: "0.8.4",
   solidity: "0.6.8",
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
   networks: {
-
-  }
+    hardhat: {
+      chainId: 1337,
+    },
+    // ropsten: {
+    //   url: "https://ropsten.infura.io/v3/projectid",
+    //   accounts: [process.env.a2key]
+    // },
+    // rinkeby: {
+    //   url: "https://rinkeby.infura.io/v3/projectid",
+    //   accounts: [process.env.a2key]
+    // }
+  },
 
 };
